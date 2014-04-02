1ec98fbc4d3d99b3c95dbe9f2982f335bbcbb9ef
Richard Groves <richard@noodlfroot.com>
2014-04-02 15:24:07 +0100

    Initial Commit

---
+++ Cocos2d-v3-ScissorNode-Test/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Cocos2d-v3-ScissorNode-Test
+//
+//  Created by Richard Groves on 02/04/2014.
+//  Copyright NoodlFroot 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Cocos2d-v3-ScissorNode-Test/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  Cocos2d-v3-ScissorNode-Test
+//
+//  Created by Richard Groves on 02/04/2014.
+//  Copyright NoodlFroot 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
